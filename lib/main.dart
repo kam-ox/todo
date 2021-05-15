55f4aa2d14852f5a4ed557b8a4810e07accbe273
kamox <kamilcc@mail.ru>
2021-05-15 11:58:40 +0600

    added theming and card wrapping listTile

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,26 @@ void main() => runApp(TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: 'Todo List', home: new TodoList());
+    return new MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        accentColor: Colors.orange.shade300,
+        primaryIconTheme: IconThemeData(color: Colors.white),
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          elevation: 8,
+          textTheme: TextTheme(
+            title: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+      title: 'Todo List',
+      home: new TodoList(),
+    );
   }
 }
 
@@ -33,9 +52,27 @@ class _TodoListState extends State<TodoList> {
   }
 
   Widget _buildTodoItem(String todoText, int index) {
-    return new ListTile(
-      title: new Text(todoText),
-      onTap: () => _promptRemoveTodoItem(index),
+    return Card(
+      shape: RoundedRectangleBorder(
+        // adding rounded corners
+        borderRadius: BorderRadius.circular(12), // use circular, not all
+      ),
+      elevation: 3,
+      margin: EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 8,
+      ),
+      child: ListTile(
+        subtitle: Text('Tap to mark as done'),
+        title: Text(todoText),
+        onTap: () => _promptRemoveTodoItem(index),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.done_all_rounded,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
     );
   }
 
@@ -82,7 +119,10 @@ class _TodoListState extends State<TodoList> {
       floatingActionButton: FloatingActionButton(
         onPressed: _pushAddTodoScreen,
         tooltip: 'Add task',
-        child: Icon(Icons.add),
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).primaryIconTheme.color,
+        ),
       ),
     );
   }
@@ -100,7 +140,7 @@ class _TodoListState extends State<TodoList> {
               Navigator.pop(context);
             },
             decoration: InputDecoration(
-              hintText: 'What you want to do?',
+              hintText: 'What to do?',
               contentPadding: EdgeInsets.all(16),
             ),
           ));
